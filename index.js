95bad1f526610211a2cbb5c14286934b04503c67
Jeremy Robles <jeremy.robles56@gmail.com>
2016-07-11 09:07:52 -0700

    updated the master branch

--- index.js
+++ index.js
@@ -5,24 +5,53 @@ var app = new alexa.app();
  * LaunchRequest.
  */
 app.launch(function(request,response) {
-	response.say('Hey there fancy pants!');
-	response.card("Hey there fancy pants!","This is an example card");
+	response.say('Cool, who has come over?');
+	// response.card("Hey there fancy pants!","This is an example card");
+	// var number = request.slot('numbers');
+	// response.say('You asked for the number '+number);
+	response.shouldEndSession(false);
+	response.send();
 });
 
 
 /**
  * IntentRequest.
  */
-app.intent('number',
+app.intent('GuestIntent',
   {
-    'slots':{'number':'NUMBER'},
-    'utterances':[ 'say the number {1-100|number}' ]
+    'slots':{'Guest':'LIST_OF_GUESTS'},
+    'utterances':[
+		'{Guest} {Guest} and {Guest}',
+		'{Guest}',
+		'{Guest} and {Guest}',
+		'{Guest} and his family',
+		'{Guest} and her family',
+		'{Guest} and his boyfriend',
+		'{Guest} and her boyfriend',
+		'{Guest} and his girlfriend',
+		'{Guest} and her girlfriend',
+		'{Guest} and his wife',
+		'{Guest} and her wife',
+		'{Guest} and his husband',
+		'{Guest} and her husband',
+		'{Guest} and his friends',
+		'{Guest} and her friends',
+		'{Guest} and friends'
+
+	]
   },
   function(request,response) {
-    var number = request.slot('number');
-    response.say('You asked for the number '+number);
-    response.shouldEndSession(true);
-    response.send();
+    var guest = request.slot('Guest');
+	  if(guest == 1){
+		  response.say('Hello '+guest+' I am happy jeremy has friends, if you would like to listen to music just ask me!');
+		  response.shouldEndSession(true);
+		  response.send();
+	  }else{
+		  response.say('Hello '+guest+' I am happy jeremy has friends, if you would like to listen to music just ask me!');
+		  response.shouldEndSession(true);
+		  response.send();
+	  }
+
   }
 );
 
@@ -30,17 +59,17 @@ app.intent('number',
 /**
  * IntentRequest w/ asynchronous response.
  */
-app.intent('checkStatus', 
+app.intent('EndIntent',
 	{
     	'utterances':[ 
-    		'status check', 'what is the status', 'tell me the status'
+    		'end', 'cancel', 'stop'
     	]
   	},
 	function(request,response) {
 		setTimeout(function() {		// simulate an async request
 
 	        // This is async and will run after a brief delay
-	        response.say('Status is operational, mam!');
+	        response.say('alright');
 	    
 	        // Must call send to end the original request
 	        response.send();
